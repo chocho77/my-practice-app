676d4b1262b2308feb97caf9e43ef70eef02da9a
chavdar.momchilov <chavmom@gmail.com>
2023-05-02 09:48:41 +0300

    On branch master  Changes to be committed: 	modified:   angular.json 	modified:   src/app/app.component.html 	modified:   src/app/app.module.ts 	new file:   src/app/navbar/navbar.component.ts

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-navbar',
+    template: `<h1>Navbar Component</h1>
+               <p>Lorem poem ipsum forem e.t.c. </p>`,
+    styles: [`h1{
+                    color:red;
+                    font-size:50px;
+                }`]
+})
+
+export class NavbarComponent {
+
+}
